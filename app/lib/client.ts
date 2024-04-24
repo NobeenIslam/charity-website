29c6e66574e33426918d4566a1b8b17165c5282d
NobeenIslam <n.islam.1@outlook.com>
2024-04-24 17:36:43 +0100

    update with environment variables

--- app/lib/client.ts
+++ app/lib/client.ts
@@ -1,9 +1,14 @@
-import { createClient } from "@sanity/client";
+import { createClient } from "next-sanity";
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const apiVersion = "2023-05-03";
+
+console.log(projectId);
 
 export const client = createClient({
-  projectId: "p4piayq8", // you can find this in sanity.json
-  dataset: "production", // or the name you chose in step 1
-  useCdn: true, // `false` if you want to ensure fresh data
-  apiVersion: "2024-04-23",
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: false,
 });
-
