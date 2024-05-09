76e03a57880c019409820621c49fd906a27af9dc
NobeenIslam <n.islam.1@outlook.com>
2024-05-09 13:23:14 +0100

    Realign for mobile and move button

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,20 +6,20 @@ export default function NotFound() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-red">
       <div className="container">
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-start sm:items-center ">
           <p className="font-bold text-2xl mb-2 ">PAGE NOT FOUND</p>
           <h2 className="text-3xl md:text-4xl mb-5 ">
             Lets get you to a better place
           </h2>
+          <Button href={"/"} className={"mb-3"} variant={"default"}>
+            Go Home
+          </Button>
           <Image
             alt="404 image"
             src="/notfound.png"
             width={500}
             height={300}
           ></Image>
-          <Button href={"/"} variant={"default"}>
-            Go Home
-          </Button>
         </div>
       </div>
     </div>
