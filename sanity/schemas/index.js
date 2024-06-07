3bef15b08693e36a901169d485c8fe14cfc47148
NobeenIslam <n.islam.1@outlook.com>
2024-06-07 12:09:55 +0100

    Update READMe and documentation

--- sanity/schemas/index.js
+++ sanity/schemas/index.js
@@ -3,7 +3,10 @@ import navbar from './documents/navbar-schema.js'
 import page from './documents/page-schema.js'
 import navItem from './misc/navItem.js'
 
+//Add document schema here
 const documentTypes = [page, navbar]
+
+//Add block schema here
 const blockTypes = [project, navItem]
 
 export const schemaTypes = [...documentTypes, ...blockTypes]
