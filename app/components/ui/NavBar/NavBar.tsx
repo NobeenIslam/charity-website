50b97dfa80a98f6f2b4ed4e64c4b7b12ec415dd3
NobeenIslam <n.islam.1@outlook.com>
2024-06-10 18:16:25 +0100

    Create navbar storybook mock and add

--- app/components/ui/NavBar/NavBar.tsx
+++ app/components/ui/NavBar/NavBar.tsx
@@ -1,11 +1,6 @@
 "use client";
 
-import {
-  NavBar as NavBarType,
-  SanityImageAsset,
-  SanityImageCrop,
-  SanityImageHotspot,
-} from "@/app/lib/schemaTypes";
+import { NavBar as NavBarType } from "@/app/lib/schemaTypes";
 import React, { useState } from "react";
 import { Button } from "../Button";
 import Image from "next/image";
@@ -14,12 +9,12 @@ import CrossSvg from "@/public/icons/cross.svg";
 import { useMediaQuery } from "@mui/material";
 import { breakpoint } from "@/app/lib/breakpoints";
 import { MobileNavMenu } from "./MobileNavMenu";
-import {client} from "@/app/lib/client";
+import { client } from "@/app/lib/client";
 import { useNextSanityImage } from "next-sanity-image";
 
 export interface NavBarProps extends NavBarType {}
 
-const NavBar = ({ navItems = [], logo  }: NavBarProps) => {
+const NavBar = ({ navItems = [], logo }: NavBarProps) => {
   const isMobile = useMediaQuery(`(max-width:${breakpoint.sm})`);
   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
 
@@ -40,12 +35,7 @@ const NavBar = ({ navItems = [], logo  }: NavBarProps) => {
     <div className="sticky top-0 z-50 flex items-center p-1 bg-white border-b border-gray-200">
       <div className="flex-shrink-0">
         {logo && (
-          <Image
-            {...imageProps}
-            width={50}
-            height={50}
-            alt="charity-logo"
-          />
+          <Image {...imageProps} width={50} height={50} alt="charity-logo" />
         )}
       </div>
       {isMobile ? (
---
+++ app/components/ui/NavBar/NavBarMock.ts
@@ -0,0 +1,32 @@
+const navBarMock = {
+  logo: {
+    _type: "image",
+    asset: {
+      _ref: "image-78df3f9388fbca65a916f1210cc296cec2b010e4-1024x1024-png",
+      _type: "reference",
+    },
+  },
+  navItems: [
+    {
+      _key: "33a77ca1847f",
+      _type: "navItem",
+      link: "/",
+      title: "Home",
+    },
+    {
+      _key: "c1d565c5df6f",
+      _type: "navItem",
+      link: "/about",
+      title: "About",
+    },
+    {
+      _key: "46bd4ce2bf38",
+      _type: "navItem",
+      link: "/projects",
+      title: "Projects",
+    },
+  ],
+  title: "Navigation Bar",
+};
+
+export { navBarMock };
