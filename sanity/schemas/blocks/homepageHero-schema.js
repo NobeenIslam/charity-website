5fd2d23e3f61c367ba6caf1b226d8d7d4400b680
NobeenIslam <n.islam.1@outlook.com>
2024-06-12 12:02:26 +0100

    Update validation for hero schema

--- sanity/schemas/blocks/homepageHero-schema.js
+++ sanity/schemas/blocks/homepageHero-schema.js
@@ -1,29 +1,31 @@
-import ctaButton from "../misc/ctaButton";
+import ctaButton from '../misc/ctaButton'
 
-export default ({
-    name: 'homepageHero',
-    title: 'Homepage Hero',
-    type: 'object',
-    fields: [
-      {
-        name: 'heading',
-        title: 'Heading',
-        type: 'string',
+export default {
+  name: 'homepageHero',
+  title: 'Homepage Hero',
+  type: 'object',
+  fields: [
+    {
+      name: 'heading',
+      title: 'Heading',
+      type: 'string',
+    },
+    {
+      title: 'Description',
+      name: 'description',
+      type: 'array',
+      of: [{type: 'block'}],
+    },
+    {
+      name: 'backgroundImage',
+      title: 'Background Image',
+      type: 'image',
+      options: {
+        hotspot: true,
       },
-      {
-        name: 'description',
-        title: 'Description',
-        type: 'text',
-      },
-      {
-        name: 'backgroundImage',
-        title: 'Background Image',
-        type: 'image',
-        options: {
-          hotspot: true,
-        },
-      },
-      ctaButton
-    ],
-  })
-  
+      codegen: { required: true },
+      validation: (Rule) => Rule.required(),
+    },
+    ctaButton,
+  ],
+}
