71df58e34da1ec3dd1f471f20328e336ee14a2db
NobeenIslam <n.islam.1@outlook.com>
2024-06-13 14:50:12 +0100

    Move gutter away from layout and into component. Adjust text alignment

--- app/components/blocks/HomePageHero/HomepageHero.tsx
+++ app/components/blocks/HomePageHero/HomepageHero.tsx
@@ -19,13 +19,13 @@ export const HomepageHero = ({
 
   return (
     <div
-      className="relative flex items-center justify-center h-screen bg-cover bg-center"
+      className="gutter relative flex items-center justify-left h-screen bg-cover bg-center "
       style={{
         backgroundImage: `url(${backgroundImageProps.src})`,
       }}
     >
       <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-      <div className="relative z-10 text-center text-white p-4">
+      <div className="relative z-10 text-left text-white p-4 w-1/2 ml-4">
         <h1 className="text-4xl font-bold mb-4">{heading}</h1>
         <PortableText value={description} />
         <Button className="mt-4" aria-label={ctaButton?.buttonAccessibleLabel}>
--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,7 @@ export default async function RootLayout({
     <html lang="en">
       <body className={`${roboto.variable} font-sans`}>
         <NavBar {...navBar} />
-        <main className="gutter">{children}</main>
+        <main>{children}</main>
       </body>
     </html>
   );
