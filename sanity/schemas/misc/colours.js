49ccd311b2427cc18a6295fd1422286464492d2b
NobeenIslam <n.islam.1@outlook.com>
2024-06-14 15:51:28 +0100

    Changed colour's options to be lighter

--- sanity/schemas/misc/colours.js
+++ sanity/schemas/misc/colours.js
@@ -5,14 +5,14 @@ const coloursField = (name = 'color', title = 'Color') => {
     type: 'string',
     options: {
       list: [
-        {title: 'Red', value: 'bg-red-500'},
-        {title: 'Green', value: 'bg-green-500'},
-        {title: 'Blue', value: 'bg-blue-500'},
-        {title: 'Yellow', value: 'bg-yellow-500'},
-        {title: 'Indigo', value: 'bg-indigo-500'},
-        {title: 'Purple', value: 'bg-purple-500'},
-        {title: 'Pink', value: 'bg-pink-500'},
-        {title: 'Gray', value: 'bg-gray-500'},
+        {title: 'Red', value: 'bg-red-200'},
+        {title: 'Green', value: 'bg-green-200'},
+        {title: 'Blue', value: 'bg-blue-200'},
+        {title: 'Yellow', value: 'bg-yellow-200'},
+        {title: 'Indigo', value: 'bg-indigo-200'},
+        {title: 'Purple', value: 'bg-purple-200'},
+        {title: 'Pink', value: 'bg-pink-200'},
+        {title: 'Gray', value: 'bg-gray-200'},
         // Add more colors as needed
       ],
       layout: 'dropdown', // Ensures it appears as a dropdown in the studio
