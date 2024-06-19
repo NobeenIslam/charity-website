c367e392972f7b54df9a4c0812001de53d8cc08c
NobeenIslam <n.islam.1@outlook.com>
2024-06-19 13:08:29 +0100

    Remove height declarations on card and add decorator to card story

--- app/components/ui/Card/Card.stories.tsx
+++ app/components/ui/Card/Card.stories.tsx
@@ -1,11 +1,19 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { cardMock } from "./CardMock";
+import React from "react";
 
 import { Card } from "./Card";
 
 const meta: Meta<typeof Card> = {
   component: Card,
   args: cardMock,
+  decorators: [
+    (Story) => (
+      <div style={{ maxWidth: "400px", margin: "0 auto", padding: "1rem" }}>
+        <Story />
+      </div>
+    ),
+  ],
 };
 
 export default meta;
--- app/components/ui/Card/Card.tsx
+++ app/components/ui/Card/Card.tsx
@@ -37,15 +37,13 @@ export const Card = ({
 
   return (
     <Link href={link} passHref>
-      <div className="bg-white shadow-lg rounded-lg overflow-hidden transform transition duration-300 hover:scale-105 hover:shadow-xl cursor-pointer border-gray-500 max-w-[500px]">
+      <div className="bg-white shadow-lg rounded-lg overflow-hidden transform transition duration-300 hover:scale-105 hover:shadow-xl cursor-pointer border-gray-500">
         <div className="relative w-full">
           <Image
             {...imageProps}
             alt={title}
             objectFit="cover"
             className="rounded-t-lg"
-            width={600}
-            height={400}
           />
         </div>
         <div className="p-6 flex flex-col justify-between">
