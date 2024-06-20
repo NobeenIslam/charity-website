601527e50e5d876e1e448f9c8abee6a93fc340f2
NobeenIslam <n.islam.1@outlook.com>
2024-06-20 15:11:34 +0100

    Add show more and show less functionality

--- app/components/blocks/ProjectGrid/ProjectGrid.tsx
+++ app/components/blocks/ProjectGrid/ProjectGrid.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { PortableText } from "../../../utilities/portableText";
 import { ProjectGrid as SanityProjectGridType } from "../../../utilities/schemaTypes";
 import { Project as SanityProjectType } from "../../../utilities/schemaTypes";
 import { Card } from "@/app/components/ui/Card";
+import { Button } from "@/app/components/ui/Button";
 
 export interface ProjectGridProps extends SanityProjectGridType {
   projectCardsData: SanityProjectType[];
@@ -15,20 +16,44 @@ export const ProjectGrid = ({
   body,
   projectCardsData,
 }: ProjectGridProps) => {
-  const projectCards = projectCardsData.map((projectCardData) => {
-    const { _id, summary, cardImage, title, link, ctaButton } = projectCardData;
-    return (
-      <div key={_id} className="max-w-sm">
-        <Card
-          title={title || ""}
-          cardBody={summary || ""}
-          cardImage={cardImage}
-          link={link}
-          cta={ctaButton}
-        />
-      </div>
+  const howManyCardsAreInitiallyVisible = 3;
+  const [visibleCount, setVisibleCount] = useState(
+    howManyCardsAreInitiallyVisible
+  );
+
+  const showMoreProjects = () => {
+    setVisibleCount(
+      (howManyCardsAreCurrentlyVisible) =>
+        howManyCardsAreCurrentlyVisible + howManyCardsAreInitiallyVisible
     );
-  });
+  };
+
+  const showLessProjects = () => {
+    setVisibleCount((howManyCardsAreCurrentlyVisible) =>
+      Math.max(
+        howManyCardsAreCurrentlyVisible - howManyCardsAreInitiallyVisible,
+        howManyCardsAreInitiallyVisible
+      )
+    );
+  };
+
+  const projectCards = projectCardsData
+    .slice(0, visibleCount)
+    .map((projectCardData) => {
+      const { _id, summary, cardImage, title, link, ctaButton } =
+        projectCardData;
+      return (
+        <div key={_id} className="max-w-sm">
+          <Card
+            title={title || ""}
+            cardBody={summary || ""}
+            cardImage={cardImage}
+            link={link}
+            cta={ctaButton}
+          />
+        </div>
+      );
+    });
 
   return (
     <div className="gutter py-12">
@@ -40,6 +65,22 @@ export const ProjectGrid = ({
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 justify-center">
         {projectCards}
+      </div>
+      <div className="flex justify-center mt-8 space-x-4">
+        {visibleCount < projectCardsData.length && (
+          <div className="flex justify-center mt-8">
+            <Button onClick={showMoreProjects} variant="secondary">
+              Show More
+            </Button>
+          </div>
+        )}
+        {visibleCount > howManyCardsAreInitiallyVisible && (
+          <div className="flex justify-center mt-8">
+            <Button onClick={showLessProjects} variant="secondary">
+              Show Less
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   );
