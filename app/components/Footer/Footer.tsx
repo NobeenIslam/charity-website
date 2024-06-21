de8c0c8c4d8bbb6b19ed85cc04ce19b484d195a3
NobeenIslam <n.islam.1@outlook.com>
2024-06-21 19:06:21 +0100

    Begin styling footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -2,11 +2,37 @@
 
 import { Footer as FooterType } from "../../utilities/schemaTypes";
 import React from "react";
+import { Button } from "../ui/Button";
+import { client } from "@/app/utilities/client";
+import { useNextSanityImage } from "next-sanity-image";
 
 export interface FooterProps extends FooterType {}
 
-const Footer = ({}: FooterProps) => {
-  return <div>Footer</div>;
+const Footer = ({ mainText, footerLinks, socialLinks }: FooterProps) => {
+  const links = footerLinks?.map((footerLink) => {
+    return (
+      <Button key={footerLink._key} href={footerLink.link}>
+        {footerLink.title}|
+      </Button>
+    );
+  });
+
+  // const iconLinks = socialLinks?.map((socialLink) => {
+  //   const imageProps = useNextSanityImage(client, logo, {});
+
+  //   return (
+  //     <Button key={socialLink._key} href={socialLink.link}>
+  //       <Image></Image>
+  //     </Button>
+  //   );
+  // });
+
+  return (
+    <div>
+      <div>{mainText}</div>
+      <div>{links}</div>
+    </div>
+  );
 };
 
 export { Footer };
