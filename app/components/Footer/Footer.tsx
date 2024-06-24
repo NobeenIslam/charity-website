8cca2d9a946e92b9dc142727888c3f4f951a3678
NobeenIslam <n.islam.1@outlook.com>
2024-06-24 17:51:22 +0100

    Build and Style footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -3,35 +3,55 @@
 import { Footer as FooterType } from "../../utilities/schemaTypes";
 import React from "react";
 import { Button } from "../ui/Button";
-import { client } from "@/app/utilities/client";
+import { client } from "../../utilities/client";
 import { useNextSanityImage } from "next-sanity-image";
+import Image from "next/image";
 
 export interface FooterProps extends FooterType {}
 
 const Footer = ({ mainText, footerLinks, socialLinks }: FooterProps) => {
-  const links = footerLinks?.map((footerLink) => {
-    return (
-      <Button key={footerLink._key} href={footerLink.link}>
-        {footerLink.title}|
+  const links = footerLinks?.map((footerLink, index) => (
+    <React.Fragment key={footerLink._key}>
+      <Button
+        href={footerLink.link}
+        variant="link"
+        className=" text-gray-400 hover:text-gray-800"
+      >
+        {footerLink.title}
       </Button>
-    );
-  });
+      {index < footerLinks.length - 1 && (
+        <span className="text-gray-400 mx-1">|</span>
+      )}
+    </React.Fragment>
+  ));
 
-  // const iconLinks = socialLinks?.map((socialLink) => {
-  //   const imageProps = useNextSanityImage(client, logo, {});
+  let iconLinks: React.JSX.Element[] = [];
+  if (socialLinks) {
+    iconLinks = socialLinks.map((socialLink) => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const imageProps = useNextSanityImage(client, socialLink.icon, {});
 
-  //   return (
-  //     <Button key={socialLink._key} href={socialLink.link}>
-  //       <Image></Image>
-  //     </Button>
-  //   );
-  // });
+      return (
+        <Button
+          key={socialLink._key}
+          href={socialLink.link}
+          variant="link"
+          size="icon"
+        >
+          <Image {...imageProps} alt="social-link" className="rounded-full" />
+        </Button>
+      );
+    });
+  }
 
   return (
-    <div>
-      <div>{mainText}</div>
-      <div>{links}</div>
-    </div>
+    <footer className="bg-gray-100 py-8 px-4">
+      <div className="max-w-7xl mx-auto text-center">
+        <div className="text-gray-400 mb-4">{mainText}</div>
+        <div className="mb-4">{links}</div>
+        <div className="flex justify-center space-x-4">{iconLinks}</div>
+      </div>
+    </footer>
   );
 };
 
