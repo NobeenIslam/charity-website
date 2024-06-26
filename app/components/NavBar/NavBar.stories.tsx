abb837d2aa0b8ef1eef9009b1eed67c32245e895
nobeen <n.islam.1@outlook.com>
2024-06-26 14:14:01 +0100

    restyle nav bar to be transparent and scrollable

--- app/components/NavBar/NavBar.stories.tsx
+++ app/components/NavBar/NavBar.stories.tsx
@@ -2,10 +2,18 @@ import { Meta, StoryObj } from "@storybook/react";
 import { navBarMock } from "./NavBarMock";
 
 import { NavBar } from "./NavBar";
+import React from "react";
 
 const meta: Meta<typeof NavBar> = {
   component: NavBar,
   args: navBarMock,
+  decorators: [
+    (Story) => (
+      <div style={{ height: '150vh', overflow: 'auto', backgroundColor:'black' }}>
+        <Story />
+      </div>
+    ),
+  ],
 };
 
 export default meta;
