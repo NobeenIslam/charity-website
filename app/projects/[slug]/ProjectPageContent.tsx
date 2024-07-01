f4477326aa94b16bfdfea87f21ce557d79c19827
NobeenIslam <n.islam.1@outlook.com>
2024-07-01 16:57:40 +0100

    Fix the dynamic projects page

---
+++ app/projects/[slug]/ProjectPageContent.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+// This component was initially just in the page.tsx. But I think there was a problem with calling
+// useNextImageFunction in a server component:
+// https://stackoverflow.com/questions/72095900/typeerror-cannot-read-properties-of-null-reading-usememo
+
+import React from "react";
+import { HomepageHero } from "../../components/blocks/HomePageHero";
+import { PortableText } from "../../utilities/portableText";
+import Image from "next/image";
+import { useNextSanityImage } from "next-sanity-image";
+import { client } from "../../utilities/client";
+import { ProjectPageType } from "@/app/queries/queryTypes";
+
+export interface ProjectPageContentProps {
+  projectPost: ProjectPageType;
+}
+
+const ProjectPageContent = ({ projectPost }: ProjectPageContentProps) => {
+  const { postImages } = projectPost.page;
+
+  const postImageProps =
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    postImages?.map((postImage) => useNextSanityImage(client, postImage)) || [];
+
+  return (
+    <div>
+      <HomepageHero
+        heading={projectPost.title}
+        backgroundImage={projectPost.card?.image}
+        _type="homepageHero"
+      />
+      {projectPost.page.primaryTextSection && (
+        <PortableText value={projectPost.page.primaryTextSection} />
+      )}
+      {postImageProps.length >= 1 && (
+        <Image
+          {...postImageProps[0]}
+          alt={"image"}
+          objectFit="cover"
+          className="rounded-t-lg"
+        />
+      )}
+      {projectPost.page.secondaryTextSection && (
+        <PortableText value={projectPost.page.secondaryTextSection} />
+      )}
+      {postImageProps.length === 2 && (
+        <Image
+          {...postImageProps[1]}
+          alt={"image"}
+          objectFit="cover"
+          className="rounded-t-lg"
+        />
+      )}
+    </div>
+  );
+};
+
+export { ProjectPageContent };
--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,62 +1,24 @@
-import React from "react";
 import {
   getProjectBySlug,
   getProjectSlugs,
 } from "../../queries/queryFunctions";
-import { HomepageHero } from "../../components/blocks/HomePageHero";
-import { PortableText } from "../../utilities/portableText";
-import Image from "next/image";
-import { useNextSanityImage } from "next-sanity-image";
-import { client } from "../../utilities/client";
+import { ProjectPageContent } from "./ProjectPageContent";
+
+export const dynamicParams = false;
+//Dynamic segments not included in generateStaticParams will return a 404.
 
 export async function generateStaticParams() {
   const postSlugs = await getProjectSlugs();
 
   return postSlugs.map((postSlug) => ({
-    slug: postSlug.slug,
+    slug: postSlug.slug?.current,
   }));
 }
-
 
 export default async function Page({ params }: { params: { slug: string } }) {
   const { slug } = params;
 
   const projectPost = await getProjectBySlug(slug);
 
-  const { postImages } = projectPost.page;
-
-  const postImageProps =
-    postImages?.map((postImage) => useNextSanityImage(client, postImage)) || [];
-
-  return (
-    <div>
-      <HomepageHero
-        heading={projectPost.title}
-        backgroundImage={projectPost.card?.image}
-        _type="homepageHero"
-      />
-      {projectPost.page.primaryTextSection && (
-        <PortableText value={projectPost.page.primaryTextSection} />
-      )}
-      {postImageProps.length >= 1 && (
-        <Image
-          {...postImageProps[0]}
-          alt={"image"}
-          objectFit="cover"
-          className="rounded-t-lg"
-        />
-      )}
-      {projectPost.page.secondaryTextSection && (
-        <PortableText value={projectPost.page.secondaryTextSection} />
-      )}
-      {postImageProps.length === 2 && (
-        <Image
-          {...postImageProps[1]}
-          alt={"image"}
-          objectFit="cover"
-          className="rounded-t-lg"
-        />
-      )}
-    </div>
-  );
+  return <ProjectPageContent projectPost={projectPost} />;
 }
