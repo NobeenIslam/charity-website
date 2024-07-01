30d6f96e34ee94ba832878ff111a0615d3ea2188
NobeenIslam <n.islam.1@outlook.com>
2024-07-01 19:07:49 +0100

    Style text content on project page

--- app/projects/[slug]/ProjectPageContent.tsx
+++ app/projects/[slug]/ProjectPageContent.tsx
@@ -25,33 +25,47 @@ const ProjectPageContent = ({ projectPost }: ProjectPageContentProps) => {
 
   return (
     <div>
+      {" "}
       <HomepageHero
         heading={projectPost.title}
         backgroundImage={projectPost.image}
         _type="homepageHero"
       />
-      {projectPost.page.primaryTextSection && (
-        <PortableText value={projectPost.page.primaryTextSection} />
-      )}
-      {postImageProps.length >= 1 && (
-        <Image
-          {...postImageProps[0]}
-          alt={"image"}
-          objectFit="cover"
-          className="rounded-t-lg"
-        />
-      )}
-      {projectPost.page.secondaryTextSection && (
-        <PortableText value={projectPost.page.secondaryTextSection} />
-      )}
-      {postImageProps.length === 2 && (
-        <Image
-          {...postImageProps[1]}
-          alt={"image"}
-          objectFit="cover"
-          className="rounded-t-lg"
-        />
-      )}
+      <div className="max-w-7xl my-8 mx-auto p-8 sm:p-10 lg:p-12">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          {projectPost.page.primaryTextSection && (
+            <div className="prose max-w-none">
+              <PortableText value={projectPost.page.primaryTextSection} />
+            </div>
+          )}
+          {postImageProps.length >= 1 && (
+            <div>
+              {" "}
+              <Image
+                {...postImageProps[0]}
+                alt={"image"}
+                objectFit="cover"
+                className="my-8"
+              />
+            </div>
+          )}
+          {projectPost.page.secondaryTextSection && (
+            <div className="prose max-w-none">
+              <PortableText value={projectPost.page.secondaryTextSection} />
+            </div>
+          )}
+          {postImageProps.length === 2 && (
+            <div>
+              <Image
+                {...postImageProps[1]}
+                alt={"image"}
+                objectFit="cover"
+                className=" my-8"
+              />
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
