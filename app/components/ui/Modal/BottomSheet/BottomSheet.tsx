b9dca325ec30b9c064bbf5b416b5aa2e6c3da173
nobeen <n.islam.1@outlook.com>
2024-07-12 20:38:16 +0100

    Fix font problem for bottom sheet in storybook

--- app/components/ui/Modal/BottomSheet/BottomSheet.tsx
+++ app/components/ui/Modal/BottomSheet/BottomSheet.tsx
@@ -4,6 +4,7 @@ import { Sheet } from "react-modal-sheet";
 import { X } from "lucide-react";
 import { Button } from "../../Button";
 import { ModalProps } from "../SideBarBottomSheet";
+import { fontVariable } from "../../../../styles";
 
 export const BottomSheet: React.FC<ModalProps> = ({
   isOpen,
@@ -13,10 +14,10 @@ export const BottomSheet: React.FC<ModalProps> = ({
 }) => {
   return (
     <Sheet isOpen={isOpen} onClose={onClose}>
-      <Sheet.Container className="font-sans">
+      <Sheet.Container style={{ fontFamily: "Montserrat, sans-serif" }}>
         <Sheet.Header>
           <div className="flex justify-between items-center p-4">
-            <h2 className="text-xl font-bold">{title}</h2>
+            <h2 className={`text-xl font-bold`}>{title}</h2>
             <Button
               onClick={onClose}
               className="text-gray-500 hover:text-gray-700"
