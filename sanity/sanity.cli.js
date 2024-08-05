4d0dd2edee3ff4fd9769521a9a941f2fab6d1652
NobeenIslam <n.islam.1@outlook.com>
2024-08-05 14:21:19 +0100

    Update method of choosing sanity dataset from env variables

--- sanity/sanity.cli.js
+++ sanity/sanity.cli.js
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'p4piayq8',
-    dataset: 'production',
+    dataset: process.env.NODE_ENV === 'development' ? 'development' : 'production', //development and production are the actual names of the datasets
   },
 })
--- utilities/client.ts
+++ utilities/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET; //Has been set to production on vercel
+const dataset = process.env.NODE_ENV === 'development' ? 'development' : 'production'; //development and production are the actual names of the datasets
 const apiVersion = "2023-03-04";
 
 export const client = createClient({
