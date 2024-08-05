898c1263d1ba552246ee36cd01fa4047d557166c
NobeenIslam <n.islam.1@outlook.com>
2024-08-05 13:21:59 +0100

    Add comment

--- utilities/client.ts
+++ utilities/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET; //Has been set to production on vercel
 const apiVersion = "2023-03-04";
 
 export const client = createClient({
